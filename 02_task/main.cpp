717d161593e6471f359f9f7a2c55c9490da3d013
Evgenii Blinov <scythian89@gmail.com>
2025-09-16 00:04:26 +0300

    init commit

---
+++ 02_task/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+
+void find_pair(int array[], int res, int size) {
+    bool find = false;
+    for (int i = 0; i < size; i++) {
+        for (int j = 1; j < size; j++) {
+            if (array[i] + array[j] == res) {
+                std::cout << array[i] << " " << array[j] << std::endl;
+                return;
+            }
+        }
+    }
+
+    if (!find) {
+        std::cout << "pair not found";
+    }
+}
+
+int main() {
+    std::cout << "===Task_02===\n";
+
+    int array[] = {2, 7, 11, 15};
+    int res = 9;
+
+    find_pair(array, res, 4);
+}
